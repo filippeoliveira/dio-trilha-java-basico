34054ad6c037b1c638654e03dd62cc5b2b67764a
filippeoliveira <filippeoliveira@gmail.com>
2024-09-10 18:16:44 -0300

    finalização de projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,24 +4,25 @@ public class ContaTerminal {
 
     //Toda variavel que fora denominada static, foi  para poder ser acessada por todos os metodos static
 
-    String agencia = "067-8";
-    int numero = 1021;
-    String nomeCliente = "Mario Andrade";
+    static String agencia = "067-8";
+    static int numero = 1021;
+    static String nomeCliente = "Mario Andrade";
     static double saldo = 237.48;
     static Scanner entrada = new Scanner(System.in);
-    int inicio = 0;
-    String cpf = "0";
+    static int inicio = 0;
+    static String cpf = "0";
     
-    public void main (String[] args) {
+    public static void main (String[] args) {
       
         System.out.println("Bem vindo ao seu banco, se não possui uma conta, clique em criar!");
-        escolha1();
-    
+        escolha1();    
     }
+   
+
     
     //criei um metodo para ponto de retorno inicial caso não houvesse cadastro
 
-     public void escolha1(){
+     public static void escolha1(){
 
         System.out.println("-------------------------------\n"+
                            "   Selecione a opção desejada  \n"+
@@ -29,34 +30,52 @@ public class ContaTerminal {
                            "     1 - Acessar Conta         \n"+ 
                            "     2 - Criar Nova Conta      \n"+
                            "---------------------------------");
-        inicio=entrada.nextInt();
+        int inicio=entrada.nextInt();
         
             switch (inicio) {
                 
                 case 1:
                     if(cpf!="0"){
-                        System.out.println("Por favor Digite os dados solicitados");      
-                        agencia=entrada.next();
-                        System.out.println("Digite sua Conta");
-                        numero=entrada.nextInt();
-                            if (agencia.equals("067-8") && numero==1021){
-                                 escolha2();
+                        System.out.println("Por favor Digite a agência");      
+                        String agenciaDigitada=entrada.next();
+                            if (agenciaDigitada.equals(agencia)){
+                                System.out.println("Digite sua Conta");
+                                int numeroDigitado=entrada.nextInt();
+                                    if(numeroDigitado==numero){
+                                pressKey(); 
+                                escolha2();
+                                break;
+
+                                    }else{
+                                        System.out.println("Conta não encontrada");
+                                        pressKey();
+                                        escolha1();
+                                        break;
+                                        }
 
                             }else{
-                                 System.out.println("Conta não reconhecida");           
-                                 escolha1();
-                                 break;
-                                 }
+                                System.out.println("Agência não encontrada");
+                                pressKey();
+                                escolha1();
+                                break;
+                                }
+                                
+
                     
-                         } else {
+                    
+                    } else {
                         System.out.println("Você ainda não possui conta no banco");
+                        pressKey();
                         escolha1();
+                        break;
                          }
+
                          
                 case 2:
                         System.out.println("Por favor insira seu cpf");
                         cpf= entrada.next();
-                        System.out.println("Bem vindo Mario Andrade! Anote sua AGÊNCIA :"+agencia+" e CONTA :"+numero );
+                        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta é" +numero+ " e seu saldo é "+saldo+" já está disponível para saque.");
+                        pressKey();
                         escolha1();
                     break;
 
@@ -83,33 +102,39 @@ public class ContaTerminal {
                 switch (escolha) {
                 
                     case 1:
+                    
+                        System.out.println("Digite o valor que deseja depositar");
                         double deposito = entrada.nextDouble();
                         saldo = saldo + deposito;
                         System.out.println("Seu novo saldo é :" + saldo);
                         pressKey();
                         escolha2();              
-                    break;
+                        break;
+                       
                                    
                     case 2:
+                    System.out.println("Digite o valor que deseja sacar");
                         double saque = entrada.nextDouble();
+
                         if (saldo>=saque){
-                        saldo = saldo - saque;
-                        System.out.println("Seu novo saldo é : "+saldo);
-                        pressKey();
-                        escolha2();
+                            saldo = saldo - saque;
+                            System.out.println("Seu novo saldo é : "+saldo);
+                            pressKey();
+                            escolha2();
+                            break;
 
                         }else{
-                        System.out.println("Você não tem saldo o suficiente");
-                        }
-                        pressKey();
-                        escolha2();
-                    break;
+                            System.out.println("Você não tem saldo o suficiente");
+                            pressKey();
+                            escolha2();
+                            break;
+                            }
 
                     case 3:
                         System.out.println("Seu saldo é :"+saldo);
                         pressKey();
                         escolha2();
-                    break;
+                        break;
 
                     case 4:
                         System.out.println("Obrigado por utilizar nosso banco!");
@@ -126,16 +151,23 @@ public class ContaTerminal {
 
           //metodo para pressionamento de tecla com captura de exceção
           private static void pressKey() {
+            System.out.println("\npressione uma tecla para continuar");
             try { System.in.read();
                 for(int clear = 0; clear < 50; clear++)
                 {
                      System.out.println("\b") ;
                 }
+                
             }
             catch(Exception ex) {
                 System.out.println(ex);
+              
+
             } 
-          }
+        
+       
+        }
+        
         }
 
         
